782b8429cd2bc3af265c3a2969acc15e9ff53a32
BirukL137 <buraf13k@gmail.com>
2023-04-27 13:43:28 +0300

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords.
+"""
+
+from bcrypt import gensalt, hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    This function that expects one string argument name password
+    and returns salted, hashed password, which is a byte string.
+    """
+    salt = gensalt()
+    hashed_password = hashpw(password.encode(), salt)
+    return hashed_password
