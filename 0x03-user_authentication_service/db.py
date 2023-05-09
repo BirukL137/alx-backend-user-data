19e3966bb9cbd08d9f2e8f3c8fdca4cfde1939cb
BirukL137 <buraf13k@gmail.com>
2023-05-09 23:05:34 +0300

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,7 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -60,7 +61,7 @@ class DB:
             if i not in col_names:
                 raise InvalidRequestError
 
-        user = session.query(User).filter_by(**kwargs).first()
+        user = session.query(User).filter_by(**kwargs).one()
         if user is None:
             raise NoResultFound
         return user
