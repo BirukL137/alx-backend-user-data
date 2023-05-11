852f2e6912cefe9309cf61578e6888606a807dbd
BirukL137 <buraf13k@gmail.com>
2023-05-11 11:03:19 +0300

    19. Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,6 +6,7 @@ Log in
 Log out
 user profile
 Get reset password token
+Update password end-point
 """
 
 from flask import Flask, jsonify, request, abort, redirect
@@ -100,6 +101,24 @@ def get_reset_password_token():
     try:
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """
+    A method that takes email, reset token and hashed_password request form to
+    update the password with the new one. If the token is invalid, it will
+    catch an exception and responds with 403 HTTP code. Otherwise, it responds
+    with a 200 HTTP code and JSON payload.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("hashed_password")
+    try:
+        AUTH.update_password(reset_token=reset_token, password=new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
 
